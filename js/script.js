abc1b02fbd496cf90172dc26aad1445e81b0484d
Marsel Kaliev <marsellkaliev7722@gmail.com>
2021-02-26 16:45:46 +0500

    add loops and if

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
@@ -8,9 +10,33 @@ const personalMovieDB = {
 	privat: false
 };
 
-const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
-const filmRating = prompt('На сколько оцените его?', '');
+let lastFilm ='', filmRating = '';
+let i = 0;
 
-personalMovieDB.movies[lastFilm] = filmRating;
+for (let i = 0; i < numberOfFilms; i++) {
+	lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+	filmRating = prompt('На сколько оцените его?', '');
+	if (lastFilm === null || filmRating === null) {
+		alert('не нажимайте отмена! пожалуйста, введите снова.');
+		i--;
+	} else if (lastFilm === '' || filmRating === '') {
+		alert('вы ничего не ввели! пожалуйста, введите снова.');
+		i--;
+	} else if (lastFilm.length > 50 || filmRating.length > 50) {
+		alert('название фильма не должно быть больше 50 символов! пожалуйста, введите снова.');
+		i--;
+	}
+	personalMovieDB.movies[lastFilm] = filmRating;
+}
+
+if(personalMovieDB.count < 10) {
+	alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	alert('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+	alert('Вы киноман!');
+} else {
+	alert('Произошла ошибка!');
+}
 
 console.log(personalMovieDB);
