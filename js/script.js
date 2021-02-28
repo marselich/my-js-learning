4c450fa3958e8b44c964ae169ea4d6106b9dffe4
Marsel Kaliev <marsellkaliev7722@gmail.com>
2021-02-28 16:41:20 +0500

    refactoring: add methods

--- js/script.js
+++ js/script.js
@@ -1,76 +1,85 @@
 'use strict';
 
-let numberOfFilms;
 
-function start() {
-	numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-
-	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-		numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-	}
-}
-
-start();
 
 const personalMovieDB = {
-	count: numberOfFilms,
+	count: 0,
 	movies: {},
 	actors: {},
 	genres: [],
-	privat: false
+	privat: false,
+
+	start: function() {
+		this.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+		while (this.count == '' || this.count == null || isNaN(this.count)) {
+			this.count = +prompt("Сколько фильмов вы уже посмотрели?", "");
+		}
+	},
+
+	rememberMyFilms: function() {
+		for (let i = 0; i < this.count; i++) {
+			let lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+			let filmRating = prompt('На сколько оцените его?', '');
+			if (lastFilm === null || filmRating === null) {
+				alert('не нажимайте отмена! пожалуйста, введите снова.');
+				i--;
+			} else if (lastFilm === '' || filmRating === '') {
+				alert('вы ничего не ввели! пожалуйста, введите снова.');
+				i--;
+			} else if (lastFilm.length > 50 || filmRating.length > 50) {
+				alert('название фильма не должно быть больше 50 символов! пожалуйста, введите снова.');
+				i--;
+			}
+			personalMovieDB.movies[lastFilm] = filmRating;
+		}
+	},
+
+	detectPersonalLevel: function() {
+		if(personalMovieDB.count < 10) {
+			alert('Просмотрено довольно мало фильмов');
+		} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+			alert('Вы классический зритель');
+		} else if (personalMovieDB.count >= 30) {
+			alert('Вы киноман!');
+		} else {
+			alert('Произошла ошибка!');
+		}
+	},
+
+	showMyDb: function() {
+		if(!personalMovieDB.privat) {
+			console.log(personalMovieDB);
+		}
+	},
+
+	writeYourGenres: function() {
+		for(let i = 0; i < 3; i++) {
+			let genre = prompt(`Ваш любимый жанр под номером ${i+1}?`, '');
+			if (genre === null || genre === '') {
+				alert('Ошибка! Вы ввели пустую строку или нажали отмена! Введите снова.');
+				i--;
+			} else {
+				personalMovieDB.genres[i] = genre;
+			}
+		}
+		this.genres.forEach((genre, i) => {
+			console.log(`Любимый жанр #${i+1} - это ${genre}`);
+		});
+	},
+
+	toggleVisibleMyDB: function() {
+		this.privat = !this.privat;
+	}
 };
 
-let lastFilm ='', filmRating = '';
 
-function rememberMyFilms() {
-	for (let i = 0; i < numberOfFilms; i++) {
-		lastFilm = prompt('Один из последних просмотренных фильмов?', '');
-		filmRating = prompt('На сколько оцените его?', '');
-		if (lastFilm === null || filmRating === null) {
-			alert('не нажимайте отмена! пожалуйста, введите снова.');
-			i--;
-		} else if (lastFilm === '' || filmRating === '') {
-			alert('вы ничего не ввели! пожалуйста, введите снова.');
-			i--;
-		} else if (lastFilm.length > 50 || filmRating.length > 50) {
-			alert('название фильма не должно быть больше 50 символов! пожалуйста, введите снова.');
-			i--;
-		}
-		personalMovieDB.movies[lastFilm] = filmRating;
-	}
-}
-
-rememberMyFilms();
-
-function detectPersonalLevel() {
-	if(personalMovieDB.count < 10) {
-		alert('Просмотрено довольно мало фильмов');
-	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-		alert('Вы классический зритель');
-	} else if (personalMovieDB.count >= 30) {
-		alert('Вы киноман!');
-	} else {
-		alert('Произошла ошибка!');
-	}
-}
-
-detectPersonalLevel();
-
-function showMyDb() {
-	if(!personalMovieDB.privat) {
-		console.log(personalMovieDB);
-	}
-}
-
-showMyDb();
-
-function writeYourGenres() {
-	for(let i = 0; i < 3; i++) {
-		personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}?`, '');
-	}
-}
-
-writeYourGenres();
+personalMovieDB.start();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.showMyDb();
+personalMovieDB.writeYourGenres();
+personalMovieDB.toggleVisibleMyDB();
 
 //колбэки
 
@@ -120,3 +129,4 @@ for (let key in options) {
 }
 
 console.dir(String); //чтобы узнать все свойства и методы объекта String работает в консоли браузера
+
